d75b453e20281acf9c0356ac53960ee75de6a921
BadreddineZatout <hb_zatout@esi.dz>
2021-07-02 19:05:27 +0100

    Authentification de medecin

--- app/src/main/java/com/example/askdocdoctor/MainActivity.kt
+++ app/src/main/java/com/example/askdocdoctor/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.askdocdoctor
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.askdoc.models.Doctor
+import com.example.askdoc.models.Patient
+import com.example.askdoc.services.RetrofitService
+import com.example.askdocdoctor.models.Auth
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val pref = getSharedPreferences("Auth", Context.MODE_PRIVATE)
+        val connected = pref.getBoolean("connected", false)
+        if(connected){
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else{
+            setContentView(R.layout.activity_main)
+            login.setOnClickListener {
+                val credentials = Auth(telephone.text.toString(), pdw.text.toString())
+                val call = RetrofitService.endpoint.AuthDoctor(credentials)
+                call.enqueue(object: Callback<Doctor> {
+                    @SuppressLint("RestrictedApi")
+                    override fun onResponse(call: Call<Doctor>, response: Response<Doctor>) {
+                        if(response?.isSuccessful){
+                            val data = response.body()!!
+                            pref.edit().putBoolean("connected", true).apply()
+                            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                            intent.putExtra("doctor", data)
+                            startActivity(intent)
+                            finish()
+                        }else{
+                            Toast.makeText(this@MainActivity, "Wrong credentials", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    @SuppressLint("RestrictedApi")
+                    override fun onFailure(call: Call<Doctor>, t: Throwable) {
+                        Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }
     }
 }
--- app/src/main/java/com/example/askdocdoctor/endpoints/Endpoint.kt
+++ app/src/main/java/com/example/askdocdoctor/endpoints/Endpoint.kt
@@ -1,4 +1,11 @@
 package com.example.askdoc.endpoints
 
+import com.example.askdoc.models.Doctor
+import com.example.askdocdoctor.models.Auth
+import retrofit2.Call
+import retrofit2.http.*
+
 interface Endpoint {
+    @POST("doctor/auth")
+    fun AuthDoctor(@Body auth:Auth):Call<Doctor>
 }
